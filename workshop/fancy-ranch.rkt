0f720fe3afee9311f8b3e4512697244b9dbf26bb
Hans-J. Schmid <knock@myopendoor.de>
2020-05-30 20:44:31 +0200

    S-Expr language

--- workshop/fancy-ranch.rkt
+++
@@ -1,8 +0,0 @@
-#lang workshop/pony
-
-R[
-  P[ firestorm "Hyaaaaar"
-  P[ rarity "I'm so shiiinyyy"
-  P[ dash "Look at dat speed!"
-  P[ dog "Guys, I'm not a pony."
-]R
--- workshop/parser.rkt
+++
@@ -1,6 +0,0 @@
-#lang brag
-ranch: [/NEWLINE*] /RANCH-BEGIN /NEWLINE ponies /RANCH-END [/NEWLINE]
-ponies: pony*
-pony: /SPACE* /PONY-BEGIN /SPACE+ name /SPACE+ cry /NEWLINE
-@name: ID
-@cry: STRING
