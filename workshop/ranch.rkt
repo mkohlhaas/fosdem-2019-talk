fe6e3489e54e70ce84426c5de3923223dbf09682
Hans-J. Schmid <knock@myopendoor.de>
2020-05-30 20:33:20 +0200

    ranch.rkt not needed any more.

--- workshop/ranch.rkt
+++
@@ -1,8 +0,0 @@
-#lang s-exp workshop/expander
-
-(ranch
-  (ponies
-    (pony firestorm "Hyaaaaar")
-    (pony rarity "I'm so shiiinyyy")
-    (pony dash "Look at dat speed!")
-    (pony dog "Guys, I'm not a pony.")))
